4201df5c8fc704dc305581e9412a36a23f0e997e
robinkool123 <2163814429@qq.com>
2026-08-10 00:37:03 +0800

    hw3

--- UCB-CS61A/lecture10 Tree Recursion/hw3.py
+++ UCB-CS61A/lecture10 Tree Recursion/hw3.py
@@ -79,6 +79,66 @@ def interleaved_sum(n, odd_func, even_func):
     def helper(k, f1, f2):
         if k > n:
             return 0
-        return f1(k) + helper(k + 1, f2, f1)
+        return f1(k) + helper(k + 1, f2, )
     return helper(1, odd_func, even_func)
-    
+
+def next_larger_coin(coin):
+    """Returns the next larger coin in order.
+    >>> next_larger_coin(1)
+    5
+    >>> next_larger_coin(5)
+    10
+    >>> next_larger_coin(10)
+    25
+    >>> next_larger_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
+
+def next_smaller_coin(coin):
+    """Returns the next smaller coin in order.
+    >>> next_smaller_coin(25)
+    10
+    >>> next_smaller_coin(10)
+    5
+    >>> next_smaller_coin(5)
+    1
+    >>> next_smaller_coin(2) # Other values return None
+    """
+    if coin == 25:
+        return 10
+    elif coin == 10:
+        return 5
+    elif coin == 5:
+        return 1
+
+def count_coins(total):
+    """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> count_coins(200)
+    1463
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])
+    True
+    """
+    def helper(total, coin):
+        if total == 0:
+            return 1
+        elif total < 0 or coin is None:
+            return 0
+        with_coin = helper(total-coin, coin)
+        without_coin = helper(total, next_larger_coin(coin))
+        return with_coin + without_coin
+    return helper(total, 1)
