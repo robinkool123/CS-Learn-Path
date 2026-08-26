99e3f9194c1c281f41fa54bfe7940c83e4dfda75
robinkool123 <2163814429@qq.com>
2026-08-26 21:49:18 +0800

    Review

--- UCB-CS61A/review2/review2.py
+++ UCB-CS61A/review2/review2.py
@@ -116,3 +116,325 @@ def has_seven(n):
         return True
     else:
         return has_seven(n // 10)
+
+def num_eights(n):
+    """Returns the number of times 8 appears as a digit of n.
+
+    >>> num_eights(3)
+    0
+    >>> num_eights(8)
+    1
+    >>> num_eights(88888888)
+    8
+    >>> num_eights(2638)
+    1
+    >>> num_eights(86380)
+    2
+    >>> num_eights(12345)
+    0
+    >>> num_eights(8782089)
+    3
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_eights',
+    ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr', 'For', 'While'])
+    True
+    """
+    if n==8:
+        return 1
+    elif n<10 and n!=8:
+        return 0
+    else:
+        return num_eights(n//10)+num_eights(n%10)
+
+def digit_distance(n):
+    """Determines the digit distance of n.
+
+    >>> digit_distance(3)
+    0
+    >>> digit_distance(777)
+    0
+    >>> digit_distance(314)
+    5
+    >>> digit_distance(31415926535)
+    32
+    >>> digit_distance(3464660003)
+    16
+    >>> from construct_check import check
+    >>> # ban all loops
+    >>> check(HW_SOURCE_FILE, 'digit_distance',
+    ...       ['For', 'While'])
+    True
+    """
+    if n<10:
+        return 0
+    else:
+        return digit_distance(n//10)+abs(n%10-n//10%10)
+
+def interleaved_sum(n, odd_func, even_func):
+    """Compute the sum odd_func(1) + even_func(2) + odd_func(3) + ..., up
+    to n.
+
+    >>> identity = lambda x: x
+    >>> square = lambda x: x * x
+    >>> triple = lambda x: x * 3
+    >>> interleaved_sum(5, identity, square) # 1   + 2*2 + 3   + 4*4 + 5
+    29
+    >>> interleaved_sum(5, square, identity) # 1*1 + 2   + 3*3 + 4   + 5*5
+    41
+    >>> interleaved_sum(4, triple, square)   # 1*3 + 2*2 + 3*3 + 4*4
+    32
+    >>> interleaved_sum(4, square, triple)   # 1*1 + 2*3 + 3*3 + 4*3
+    28
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'interleaved_sum', ['While', 'For', 'Mod']) # ban loops and %
+    True
+    """
+    if n==0:
+        return 0
+    if n%2==0:
+        return even_func(n)+interleaved_sum(n-1, odd_func, even_func)
+    elif n%2==1:
+        return odd_func(n)+interleaved_sum(n-1, odd_func, even_func)
+
+def next_larger_coin(coin):
+    """Returns the next larger coin in order.
+    >>> next_larger_coin(1)
+    5
+    >>> next_larger_coin(5)
+    10
+    >>> next_larger_coin(10)
+    25
+    >>> next_larger_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
+
+def next_smaller_coin(coin):
+    """Returns the next smaller coin in order.
+    >>> next_smaller_coin(25)
+    10
+    >>> next_smaller_coin(10)
+    5
+    >>> next_smaller_coin(5)
+    1
+    >>> next_smaller_coin(2) # Other values return None
+    """
+    if coin == 25:
+        return 10
+    elif coin == 10:
+        return 5
+    elif coin == 5:
+        return 1
+
+def count_coins(total):
+    """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> count_coins(200)
+    1463
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])
+    True
+    """
+    def limit_func(total,min_coin):
+        if total==0:
+            return 1
+        elif total<0 or min_coin is None:
+            return 0
+        with_min_coin=limit_func(total-min_coin,min_coin)
+        without_min_coin=limit_func(total,next_larger_coin(min_coin))
+        return with_min_coin+without_min_coin
+    return limit_func(total,1)
+
+def print_if(s, f):
+    """Print each element of s for which f returns a true value.
+
+    >>> print_if([3, 4, 5, 6], lambda x: x > 4)
+    5
+    6
+    >>> result = print_if([3, 4, 5, 6], lambda x: x % 2 == 0)
+    4
+    6
+    >>> print(result)  # print_if should return None
+    None
+    """
+    for x in s:
+        if f(x):
+            print(x)
+
+def close(s, k):
+    """Return how many elements of s that are within k of their index.
+
+    >>> t = [6, 2, 4, 3, 5]
+    >>> close(t, 0)  # Only 3 is equal to its index
+    1
+    >>> close(t, 1)  # 2, 3, and 5 are within 1 of their index
+    3
+    >>> close(t, 2)  # 2, 3, 4, and 5 are all within 2 of their index
+    4
+    >>> close(list(range(10)), 0)
+    10
+    """
+    count = 0
+    for i in range(len(s)):  # Use a range to loop over indices
+        if abs(i-s[i])<=k:
+            count+=1
+    return count
+
+def close_list(s, k):
+    """Return a list of the elements of s that are within k of their index.
+
+    >>> t = [6, 2, 4, 3, 5]
+    >>> close_list(t, 0)  # Only 3 is equal to its index
+    [3]
+    >>> close_list(t, 1)  # 2, 3, and 5 are within 1 of their index
+    [2, 3, 5]
+    >>> close_list(t, 2)  # 2, 3, 4, and 5 are all within 2 of their index
+    [2, 4, 3, 5]
+    """
+    return [s[i] for i in range(len(s)) if abs(i-s[i])<=k]
+
+from math import sqrt
+
+def squares(s):
+    """Returns a new list containing square roots of the elements of the
+    original list that are perfect squares.
+
+    >>> seq = [8, 49, 8, 9, 2, 1, 100, 102]
+    >>> squares(seq)
+    [7, 3, 1, 10]
+    >>> seq = [500, 30]
+    >>> squares(seq)
+    []
+    """
+    return [int(sqrt(n)) for n in s if sqrt(n)==round(sqrt(n))]
+
+def double_eights(n):
+    """ Returns whether or not n has two digits in row that
+    are the number 8. Assume n has at least two digits in it.
+
+    >>> double_eights(1288)
+    True
+    >>> double_eights(880)
+    True
+    >>> double_eights(538835)
+    True
+    >>> double_eights(284682)
+    False
+    >>> double_eights(588138)
+    True
+    >>> double_eights(78)
+    False
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(LAB_SOURCE_FILE, 'double_eights', ['While', 'For'])
+    True
+    """
+    if n%100==88:
+        return True
+    if n<10:
+        return False
+    return double_eights(n//10)
+
+def make_onion(f, g):
+    """Return a function can_reach(x, y, limit) that returns
+    whether some call expression containing only f, g, and x with
+    up to limit calls will give the result y.
+
+    >>> up = lambda x: x + 1
+    >>> double = lambda y: y * 2
+    >>> can_reach = make_onion(up, double)
+    >>> can_reach(5, 25, 4)      # 25 = up(double(double(up(5))))
+    True
+    >>> can_reach(5, 25, 3)      # Not possible
+    False
+    >>> can_reach(1, 1, 0)      # 1 = 1
+    True
+    >>> add_ing = lambda x: x + "ing"
+    >>> add_end = lambda y: y + "end"
+    >>> can_reach_string = make_onion(add_ing, add_end)
+    >>> can_reach_string("cry", "crying", 1)      # "crying" = add_ing("cry")
+    True
+    >>> can_reach_string("un", "unending", 3)     # "unending" = add_ing(add_end("un"))
+    True
+    >>> can_reach_string("peach", "folding", 4)   # Not possible
+    False
+    """
+    def can_reach(x, y, limit):
+        if limit < 0:
+            return False
+        elif x == y:
+            return True
+        else:
+            return can_reach(f(x),y,limit - 1) or can_reach(g(x),y,limit - 1)
+    return can_reach
+
+def paths(m, n):
+    """Return the number of paths from one corner of an
+    M by N grid to the opposite corner.
+
+    >>> paths(2, 2)
+    2
+    >>> paths(5, 7)
+    210
+    >>> paths(117, 1)
+    1
+    >>> paths(1, 157)
+    1
+    """
+    if n==1 or m==1:
+        return 1
+    else:
+        return paths(m-1,n)+paths(m,n-1)
+
+def max_product(s):
+    """Return the maximum product of non-consecutive elements of s.
+
+    >>> max_product([10, 3, 1, 9, 2])   # 10 * 9
+    90
+    >>> max_product([5, 10, 5, 10, 5])  # 5 * 5 * 5
+    125
+    >>> max_product([])                 # The product of no numbers is 1
+    1
+    """
+    if s==[]:
+        return 1
+    else:
+        return max(max_product(s[1:]),s[0]*max_product(s[2:]))
+
+def sums(n, m):
+    """Return lists that sum to n containing positive numbers up to m that
+    have no adjacent repeats.
+
+    >>> sums(5, 1)
+    []
+    >>> sums(5, 2)
+    [[2, 1, 2]]
+    >>> sums(5, 3)
+    [[1, 3, 1], [2, 1, 2], [2, 3], [3, 2]]
+    >>> sums(5, 5)
+    [[1, 3, 1], [1, 4], [2, 1, 2], [2, 3], [3, 2], [4, 1], [5]]
+    >>> sums(6, 3)
+    [[1, 2, 1, 2], [1, 2, 3], [1, 3, 2], [2, 1, 2, 1], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+    """
+    if n < 0:
+        return []
+    if n == 0:
+        sums_to_zero = []     # The only way to sum to zero using positives
+        return [sums_to_zero] # Return a list of all the ways to sum to zero
+    result = []
+    for k in range(1, m + 1):
+        result = result + [ [k]+rest for rest in sums(n-k,m) if rest == [] or rest[0]!=k ]
+    return result
