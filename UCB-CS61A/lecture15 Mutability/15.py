617b06ac4c61b1bda245eaf97fe3f6fb4991710b
robinkool123 <2163814429@qq.com>
2026-08-31 12:15:26 +0800

    lecture15

--- UCB-CS61A/lecture15 Mutability/15.py
+++ UCB-CS61A/lecture15 Mutability/15.py
@@ -26,3 +26,146 @@ def deep_map(f, s):
             deep_map(f,s[i])
          else:
             s[i]=f(s[i])
+
+def mobile(left, right):
+    """Construct a mobile from a left arm and a right arm."""
+    assert is_arm(left), "left must be an arm"
+    assert is_arm(right), "right must be an arm"
+    return ['mobile', left, right]
+
+def is_mobile(m):
+    """Return whether m is a mobile."""
+    return type(m) == list and len(m) == 3 and m[0] == 'mobile'
+
+def left(m):
+    """Select the left arm of a mobile."""
+    assert is_mobile(m), "must call left on a mobile"
+    return m[1]
+
+def right(m):
+    """Select the right arm of a mobile."""
+    assert is_mobile(m), "must call right on a mobile"
+    return m[2]
+
+def arm(length, mobile_or_planet):
+    """Construct an arm: a length of rod with a mobile or planet at the end."""
+    assert is_mobile(mobile_or_planet) or is_planet(mobile_or_planet)
+    return ['arm', length, mobile_or_planet]
+
+def is_arm(s):
+    """Return whether s is an arm."""
+    return type(s) == list and len(s) == 3 and s[0] == 'arm'
+
+def length(s):
+    """Select the length of an arm."""
+    assert is_arm(s), "must call length on an arm"
+    return s[1]
+
+def end(s):
+    """Select the mobile or planet hanging at the end of an arm."""
+    assert is_arm(s), "must call end on an arm"
+    return s[2]
+
+def planet(mass):
+    """Construct a planet of some mass."""
+    assert mass > 0
+    return ['planet',mass]
+
+def mass(p):
+    """Select the mass of a planet."""
+    assert is_planet(p), 'must call mass on a planet'
+    return p[1]
+
+def is_planet(p):
+    """Whether p is a planet."""
+    return type(p) == list and len(p) == 2 and p[0] == 'planet'
+
+def examples():
+    t = mobile(arm(1, planet(2)),
+               arm(2, planet(1)))
+    u = mobile(arm(5, planet(1)),
+               arm(1, mobile(arm(2, planet(3)),
+                             arm(3, planet(2)))))
+    v = mobile(arm(4, t), arm(2, u))
+    return t, u, v
+
+def total_mass(m):
+    """Return the total mass of m, a planet or mobile.
+
+    >>> t, u, v = examples()
+    >>> total_mass(t)
+    3
+    >>> total_mass(u)
+    6
+    >>> total_mass(v)
+    9
+    """
+    if is_planet(m):
+        return mass(m)
+    else:
+        assert is_mobile(m), "must get total mass of a mobile or a planet"
+        return total_mass(end(left(m))) + total_mass(end(right(m)))
+
+def balanced(m):
+    """Return whether m is balanced.
+
+    >>> t, u, v = examples()
+    >>> balanced(t)
+    True
+    >>> balanced(v)
+    True
+    >>> p = mobile(arm(3, t), arm(2, u))
+    >>> balanced(p)
+    False
+    >>> balanced(mobile(arm(1, v), arm(1, p)))
+    False
+    >>> balanced(mobile(arm(1, p), arm(1, v)))
+    False
+    >>> from construct_check import check
+    >>> # checking for abstraction barrier violations by banning indexing
+    >>> check(HW_SOURCE_FILE, 'balanced', ['Index'])
+    True
+    """
+    left_moment=total_mass(end(left(m)))*length(left(m))
+    right_moment=total_mass(end(right(m)))*length(right(m))
+    if left_moment==right_moment:
+        return True
+    else:
+        return False
+
+def tree(label,branches=[]):
+    for branch in branches:
+        assert is_tree(branch),'branches must be trees'
+    return [label]+list(branches)
+
+def label(tree):
+    return tree[0]
+
+def branches(tree):
+    return tree[1:]
+
+def is_tree(tree):
+    if type(tree)!=list or len(tree)<1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+def is_leaf(tree):
+    return not branches(tree)
+
+
+def max_path_sum(t):
+    """Return the maximum root-to-leaf path sum of a tree.
+    >>> t = tree(1, [tree(5, [tree(1), tree(3)]), tree(10)])
+    >>> max_path_sum(t) # 1, 10
+    11
+    >>> t2 = tree(5, [tree(4, [tree(1), tree(3)]), tree(2, [tree(10), tree(3)])])
+    >>> max_path_sum(t2) # 5, 2, 10
+    17
+    """
+    if is_leaf(branches(t)):
+        return t[0]
+    else:
+        return max([label(t)+max_path_sum(s) for s in branches(t)])
